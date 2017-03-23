9642b0ea8717054ed59368656d755f587b2d59c8
Tushar Saxena <tushar.saxena@tothenew.com>
2017-03-23 18:08:14 +0530

    added core plugin

---
+++ youndevice-core/src/main/java/com/youndevice/core/YoundeviceCoreApplication.java
@@ -0,0 +1,12 @@
+package com.youndevice.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoundeviceCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoundeviceCoreApplication.class, args);
+	}
+}
