c39e2414f287eef26de45c2e91ae0c7e8cc29a93
Tushar Saxena <tushar.saxena@tothenew.com>
2017-04-04 15:43:57 +0530

    allowed origin in cors

--- youndevice-rest/src/main/java/com/youndevice/rest/controller/UserController.java
+++ youndevice-rest/src/main/java/com/youndevice/rest/controller/UserController.java
@@ -6,12 +6,10 @@ import com.youndevice.rest.dto.UserDTO;
 import com.youndevice.rest.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/user")
 public class UserController extends BaseController {
 
