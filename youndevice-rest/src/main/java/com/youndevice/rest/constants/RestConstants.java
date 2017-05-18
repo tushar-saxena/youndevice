84843448c8509a3d14d4ab436d299c3702aae90f
anuj1708 <anujgargcse@gmail.com>
2017-05-18 21:59:14 +0530

    Add firebase auth

--- youndevice-rest/src/main/java/com/youndevice/rest/constants/RestConstants.java
+++ youndevice-rest/src/main/java/com/youndevice/rest/constants/RestConstants.java
@@ -6,4 +6,5 @@ public class RestConstants {
     public static final String FIRST_NAME = "firstName";
     public static final String LAST_NAME = "lastName";
     public static final String AUTH_TOKEN_HEADER = "X-AUTH-TOKEN";
+    public static final String AUTH_UID = "UID";
 }
